4480a6ec5bca1d0f1aefee7e2785036041a214a6
victor <victor.s1lva@hotmail.com>
2024-06-23 12:45:15 -0400

    Adds all initial project code/configuration

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import 'dotenv/config'
+
+const app: Express = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+});
+
+app.listen(process.env.PORT, (): void => {
+  console.log('Server is running on port 3000')
+})
