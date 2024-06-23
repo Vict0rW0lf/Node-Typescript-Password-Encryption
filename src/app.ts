de55edef02169cb4eb8fa2a9d79c12e1d30ab301
victor <victor.s1lva@hotmail.com>
2024-06-23 18:08:42 -0400

    Updates main file with user routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
-import express, { Express, Request, Response } from "express";
-import 'dotenv/config'
+import express, {Express} from "express";
+import {userRoutes} from "./domain/users"
+import "dotenv/config"
 
 const app: Express = express()
 
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
-});
+app.use(userRoutes)
 
 app.listen(process.env.PORT, (): void => {
   console.log(`Server is running on port: ${process.env.PORT}`)
