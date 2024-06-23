f3575ec58c5ffac276ca0080bc07cccdf24634e9
victor <victor.s1lva@hotmail.com>
2024-06-23 12:51:16 -0400

    Updates listener to show correct PORT from env file

--- src/app.ts
+++ src/app.ts
@@ -12,5 +12,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(process.env.PORT, (): void => {
-  console.log('Server is running on port 3000')
+  console.log(`Server is running on port: ${process.env.PORT}`)
 })
